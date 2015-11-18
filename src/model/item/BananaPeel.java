677054fcd781c92b40f0af1f69ed48a72682fb26
florentplomb <plombf@gmail.com>
2015-11-18 18:19:20 +0100

    Ajout de la classe BananaPell

---
+++ src/model/item/BananaPeel.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package model.item;
+
+/**
+ * 
+ *@author Florent Plomb <plombf at gmail.com>
+ */
+public class BananaPeel extends Transportable{
+
+    public BananaPeel(String name, String description) {
+        super(name, description);
+    }
+
+}

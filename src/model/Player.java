24b0a71987770c6d70645290f954d4873c9d75df
Onur Erdogan <Chepper@a.home>
2015-11-19 17:21:22 +0100

    Player

--- src/model/Player.java
+++ src/model/Player.java
@@ -1,6 +1,7 @@
 package model;
 
 import java.util.ArrayList;
+import model.item.Item;
 
 /**
  * This class represents players in the game. Each player has a current location
@@ -10,11 +11,13 @@ public class Player {
 
     private Room currentRoom;
     private ArrayList<Room> previousRooms;
+    private ArrayList<Item> items;
 
     //This is Constructor.
     public Player() {
         currentRoom = null;
         previousRooms = new ArrayList<Room>();
+        items = new ArrayList<Item>();
     }
 
     // Return the current room for this player.
@@ -35,5 +38,13 @@ public class Player {
     // Adds the list of previously visited rooms.
     public void addPreviousRoom(Room room) {
         previousRooms.add(room);
+    }
+    
+    public void addItem(Item i){
+        items.add(i);
+    }
+    
+    public void dropItem(Item i){
+        items.remove(i);
     }
 }
